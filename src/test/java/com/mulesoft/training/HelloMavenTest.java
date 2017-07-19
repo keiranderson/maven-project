fa4b010b891c0ed0a612e414b6fd37b7663134f5
Keir Anderson <keir.anderson@concordusa.com>
2017-07-19 16:57:22 -0500

    Add tons of stuff. Most recently, handling for dynamic HTTP port value.

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,20 +1,28 @@
 package com.mulesoft.training;
 
 import static org.junit.Assert.*;
+import org.junit.Rule;
 
 import org.junit.Test;
 import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
+	@Rule
+	public DynamicPort dynamicPort = new DynamicPort("http.port");
+	
     @Test
     public void mavenFlowReturnsHelloMaven() throws Exception {
+    	System.out.println("\n\n Dynamic Port in Test Case 1: " + dynamicPort.getNumber() + "\n\n");
         runFlowAndExpect("mavenFlow", "Hello Maven");
     }
     
+    // modified to handle multiple config files
     @Override
-    protected String getConfigFile() {
-        return "maven-project.xml";
+    protected String[] getConfigFiles() {
+        String files[] = {"maven-project.xml", "global.xml"};
+    	return files;
     }
 
 }
